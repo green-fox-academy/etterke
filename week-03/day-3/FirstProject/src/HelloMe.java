31e5dee4a6784ccbe75e35436ae12028e286033f
Etterke <etterke@gmail.com>
2018-10-17 11:00:23 +0200

    Started from scratch

--- week-03/day-3/FirstProject/src/HelloMe.java
+++
@@ -1,6 +0,0 @@
-public class HelloMe {
-
-    public static void main(String[] args) {
-        System.out.println("Hello Me");
-    }
-}
--- week-03/day-3/FirstProject/src/HumptyDumpty.java
+++
@@ -1,9 +0,0 @@
-public class HumptyDumpty {
-
-    public static void main(String[] args) {
-        System.out.println("Humpty Dumpty sat on a wall");
-        System.out.println("Humpty Dumpty had a great fall");
-        System.out.println("All the King's horses and all the King's man");
-        System.out.println("Couldn't put Humpty together again");
-    }
-}
