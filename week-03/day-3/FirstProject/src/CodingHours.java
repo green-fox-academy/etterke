cef101c6d8c350b9bcd51f4681163f30a4f08f9d
Etterke <etterke@gmail.com>
2018-10-17 12:09:57 +0200

    Second batch

---
+++ week-03/day-3/FirstProject/src/CodingHours.java
@@ -0,0 +1,11 @@
+public class CodingHours {
+
+    public static void main(String[] args) {
+
+        System.out.println(("This is how many hours is spent with coding in a semester by an attendee, if the attendee only codes on workdays: ") + (6 * 17 * 5));
+
+        double percentage = 510. / 884 * 100;
+
+        System.out.println("The percentage of the coding hours in the semester if the average work hours weekly is 52: " + percentage + " %");
+    }
+}
---
+++ week-03/day-3/FirstProject/src/IntroduceYourself.java
@@ -0,0 +1,10 @@
+public class IntroduceYourself {
+
+    public static void main(String[] args) {
+
+        System.out.println("Nagy Eszter");
+        System.out.println(35);
+        System.out.println(1.74);
+
+    }
+}
