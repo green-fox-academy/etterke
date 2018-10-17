a755b8308d2c4f7fb580f0ba65b5e7a752dc07f4
Etterke <etterke@gmail.com>
2018-10-17 11:10:03 +0200

    First batch

---
+++ week-03/day-3/FirstProject/src/HelloOthers.java
@@ -0,0 +1,10 @@
+public class HelloOthers {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Viola!");
+        System.out.println("Hello, Emese!");
+        System.out.println("Hello, Erika");
+
+    }
+}
