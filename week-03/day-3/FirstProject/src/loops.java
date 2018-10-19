1aa7e37b0febd0849014565598ca2cb816712218
Etterke <etterke@gmail.com>
2018-10-19 11:40:05 +0200

    Started loops

---
+++ week-03/day-3/FirstProject/src/loops.java
@@ -0,0 +1,24 @@
+public class loops {
+
+    public static void main(String[] args) {
+
+
+        // IWontCheatOnTheExam
+
+        String line = "I won't cheat on the exam";
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(line);
+        }
+
+
+        // PrintEven
+
+        int a = 0;
+        while (a < 500) {
+            System.out.println(a);
+            a += 2;
+        }
+
+    }
+}
