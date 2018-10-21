a27f5f666bffb8d30b23800a75860969859f5d57
Etterke <etterke@gmail.com>
2018-10-21 12:01:41 +0200

    new excersises completed in loops

--- week-03/day-3/FirstProject/src/loops.java
+++ week-03/day-3/FirstProject/src/loops.java
@@ -1,4 +1,4 @@
-import java.util.Scanner
+import java.util.Scanner;
 
 public class loops {
 
@@ -25,15 +25,76 @@ public class loops {
 
         // MultiplicationTable
 
-        Scanner scanner = new Scanner (System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please write a number here: ");
         int userInput1 = scanner.nextInt();
 
+        for (int i = 0; i < 15; i++) {
+            System.out.println((i + 1) + " x " + userInput1 + " = " + (i + 1) * userInput1);
+        }
 
 
+        //CountFromTo
+
+        System.out.println("Please write two numbers here: ");
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+
+        if (number1 > number2) {
+            System.out.println("The second number should be bigger!");
+
+        } else {
+            for (int b = 0; b < (number2 - number1); b++) {
+                System.out.println(number1 + b);
+            }
+        }
 
 
+        // FizzBuzz
 
+        for (int number = 0; number < 100; number++) {
+            if (((number + 1) % 3 == 0) && ((number + 1) % 5 == 0)) {
+                System.out.println("FizzBuzz");
+
+            } else if ((number + 1) % 3 == 0) {
+                System.out.println("Fizz");
+
+            } else if ((number + 1) % 5 == 0) {
+                System.out.println("Buzz");
+
+            } else {
+                System.out.println(number + 1);
+            }
+        }
+
+
+        // DrawTriangle
+
+        System.out.println("Please write a number here: ");
+        int number3 = scanner.nextInt();
+        String star = "*";
+
+        for (int e = 0; e < number3; e++) {
+            System.out.println(star);
+            star = star + "*";
+        }
+
+
+        // DrawDiagonal
+
+        System.out.println("Please write a number here: ");
+        int rows = scanner.nextInt();
+
+        for (int e = 0; e < rows; e++) {
+            for (int f = 0; f < rows; f++) {
+                if ((f == 0) || (f == rows - 1) || (e == f) || (e == 0) || (e == rows - 1)) {
+                    System.out.print("%");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
