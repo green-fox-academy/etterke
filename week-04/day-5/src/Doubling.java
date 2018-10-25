4a8da06010b286cc64529280dde6845903384103
Etterke <etterke@gmail.com>
2018-10-25 23:49:31 +0200

    Final push

---
+++ week-04/day-5/src/Doubling.java
@@ -0,0 +1,16 @@
+public class Doubling {^
+
+    public static void main(String[] args) {
+
+        int baseNum = 123;
+        int twice = doubling(baseNum);
+
+        System.out.println("The double value is: " + twice);
+    }
+
+    public static int doubling (int n1) {
+
+        return n1 * 2;
+
+    }
+}
