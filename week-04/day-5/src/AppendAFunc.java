bdbbdda90b2f995e36b460657ca590a751a5e02c
Etterke <etterke@gmail.com>
2018-10-26 00:13:42 +0200

    One version of AppendAFunc

---
+++ week-04/day-5/src/AppendAFunc.java
@@ -0,0 +1,16 @@
+public class AppendAFunc {
+
+    public static void main(String[] args) {
+
+        String typo = "Chinchill";
+        appendAFunc(typo);
+
+    }
+
+    public static void appendAFunc (String text) {
+
+        System.out.println(text + "a");
+
+    }
+
+}
