f319ffc553504e17100d98defba637730f47b1ca
Etterke <etterke@gmail.com>
2018-10-26 11:29:51 +0200

    SumElements completed

---
+++ week-04/day-5/src/SumElements.java
@@ -0,0 +1,11 @@
+public class SumElements {
+
+    public static void main(String[] args) {
+
+        int [] r = {54, 23, 66, 12};
+        int sumOfSecondAndThird = r[1] + r[2];
+
+        System.out.printf("The sum of the second and third element is: " + sumOfSecondAndThird);
+
+    }
+}
