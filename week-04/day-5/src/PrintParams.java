ca2b347c9c90d1d24b4cb1f68899b408a1dcbfea
Etterke <etterke@gmail.com>
2018-10-26 11:12:28 +0200

    Simpler solution to PrintParams

--- week-04/day-5/src/PrintParams.java
+++ week-04/day-5/src/PrintParams.java
@@ -2,21 +2,17 @@ public class PrintParams {
 
     public static void main(String[] args) {
 
-        String input1 = "Ecc-pecc";
-        String input2 = "Kimehetsz";
-        String input3 = "Holnaputan";
-        String input4 = "Bejohetsz";
-
-        printParams(input1, input2, input3, input4);
+        printParams("Momofuku", "noodle", "and" , "milk", "bar");
 
     }
 
-    public static void printParams (String text1, String text2, String text3, String text4) {
+    public static void printParams (String... words) {
 
-        System.out.print(text1 + " ");
-        System.out.print(text2 + " ");
-        System.out.print(text3 + " ");
-        System.out.print(text4);
+        for (String words2 : words) {
+            System.out.print(words2 + " ");
+        }
+
+        System.out.println();
     }
 
 }
