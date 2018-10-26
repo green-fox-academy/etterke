49c821845e2425351f086f520ba22aa996fc3f04
Etterke <etterke@gmail.com>
2018-10-26 11:24:17 +0200

    Completed CompareLenght

---
+++ week-04/day-5/src/CompareLenght.java
@@ -0,0 +1,17 @@
+import javax.sound.midi.Soundbank;
+
+public class CompareLenght {
+
+    public static void main(String[] args) {
+
+        int [] p1 = {1, 2, 3};
+        int [] p2 = {4, 5};
+
+        if (p2.length > p1.length) {
+
+            System.out.println("p2 has more elements than p1");
+        } else {
+            System.out.println("p1 has more elements then p2");
+        }
+    }
+}
