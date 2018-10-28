626c5fac712a7fbbdf64bde1f32edfc4e1e2525f
Etterke <etterke@gmail.com>
2018-10-28 20:56:46 +0100

    Completed Append A

---
+++ week-04/day-5/src/AppendA.java
@@ -0,0 +1,12 @@
+public class AppendA {
+
+    public static void main(String[] args) {
+
+        String[] animals = {"koal", "pand", "zebr"};
+
+        for (int i = 0; i < animals.length; i++) {
+            animals[i] += "a";
+            System.out.print(animals[i] + " ");
+        }
+    }
+}
