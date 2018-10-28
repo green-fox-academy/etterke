fbcd972e08add9e3d6c786536de87a0be87def16
Etterke <etterke@gmail.com>
2018-10-28 21:09:46 +0100

    Completed Reverse and thus excersises until hard ones

---
+++ week-04/day-5/src/Reverse.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Reverse {
+
+    public static void main(String[] args) {
+
+        int[] aj = {3, 4, 5, 6 ,7};
+        int[] reverse = new int[aj.length];
+        for (int i = 0; i < reverse.length; i++) {
+            reverse[i] = aj[aj.length - 1 - i];
+
+            System.out.print((reverse[i]) + " ");
+        }
+    }
+}
