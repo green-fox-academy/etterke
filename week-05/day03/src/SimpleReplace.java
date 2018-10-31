9d5ee2a78707f03786662ac0120b1450513d339e
Etterke <etterke@gmail.com>
2018-10-31 09:39:14 +0100

    Alternative solution to SimpleReplace

--- week-05/day03/src/SimpleReplace.java
+++ week-05/day03/src/SimpleReplace.java
@@ -3,13 +3,12 @@ public class SimpleReplace {
     public static void main(String... args){
         String example = "In a dishwasher far far away";
 
-        String modifiedExample = example.replace("dishwasher", "galaxy");
 
         // I would like to replace "dishwasher" with "galaxy" in this example, but it has a problem.
         // Please fix it for me!
         // Expected output: In a galaxy far far away
 
-        System.out.println(modifiedExample);
+        System.out.println(example.replace("dishwasher", "galaxy"));
 
     }
 }
