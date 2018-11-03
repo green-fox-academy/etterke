a1f25be188dfddbd8314d8e169e87f972be32847
Etterke <etterke@gmail.com>
2018-11-03 15:03:08 +0100

    Completed Elementfinder with one method, more to follow

---
+++ week-05/day03/src/ElementFinder.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class ElementFinder {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+
+        System.out.println(containsSeven(arrayList));
+    }
+
+    public static ArrayList containsSeven(ArrayList result) {
+
+        if (result.contains(7)) {
+            System.out.println("Hoorray");
+        } else {
+            System.out.println("Noooooo");
+        }
+
+        return result;
+    }
+}
