e97c9a502aa6fb264d75b76e44d7c2227e8eb029
Etterke <etterke@gmail.com>
2018-11-03 14:38:47 +0100

    Modified UrlFixer indentation and completed AppendLetter with for loop

---
+++ week-05/day03/src/AppendLetter.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class AppendLetter {
+
+    public static void main(String[] args) {
+
+        List<String> far = Arrays.asList("bo", "anacond", "koal", "pand", "zebr");
+        // Create a method called "appendA()" that adds "a" to every string in the "far" list.
+        // The parameter should be a list.
+
+        System.out.println(appendA(far));
+
+    }
+
+    public static List<String> appendA(List x) {
+
+        ArrayList<String> farA = new ArrayList();
+
+        for (int i = 0; i < x.size(); i++) {
+            farA.add(x.get(i) + "a");
+        }
+
+        return farA;
+    }
+
+
+
+
+
+}
--- week-05/day03/src/UrlFixer.java
+++ week-05/day03/src/UrlFixer.java
@@ -7,13 +7,6 @@ public class UrlFixer {
         String url2 = url.substring(5);
         String colon = ":";
 
-
-
-        // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
-        // Also, the URL is missing a crutial component, find out what it is and insert it too!
-        // Try to solve it more than once using different String functions!
-
         System.out.println(url1 + colon + url2);
-
     }
 }
