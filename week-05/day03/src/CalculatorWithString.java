4d13ffd968180d2f6b6a785fc03fd862d4bf2b97
Etterke <etterke@gmail.com>
2018-11-06 10:44:06 +0100

    intendation corrected

--- week-05/day03/src/CalculatorWithString.java
+++ week-05/day03/src/CalculatorWithString.java
@@ -15,9 +15,13 @@ public class CalculatorWithString {
     public static Integer calculate(String operation) {
 
         int result = 0;
+
+        //Splits the string at the commas, creating 3 part, and putting those in an Array
         String[] operationArray = operation.split(",");
+        //Putting the elements of the Array into a list
         ArrayList<String> operationArrayList = new ArrayList<>(Arrays.asList(operationArray));
 
+        //Convert the string elements into integers to be able to perform mathematical operations
         int number1 = Integer.parseInt(operationArrayList.get(1));
         int number2 = Integer.parseInt(operationArrayList.get(2));
 
