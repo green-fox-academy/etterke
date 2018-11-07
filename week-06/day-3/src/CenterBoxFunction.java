ea279523b282e7dc1295a9c15d72d2dc647fee3d
Etterke <etterke@gmail.com>
2018-11-07 11:29:31 +0100

    Completed CenterBoxFunction

---
+++ week-06/day-3/src/CenterBoxFunction.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import java.awt.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class CenterBoxFunction {
+
+    public static void mainDraw(Graphics graphics){
+        for (int i = 0; i < 3; i++) {
+            drawSquare(graphics);
+        }
+    }
+
+    public static int randomColor(){
+        return ((int)(Math.random() * 256));
+    }
+
+    public static int randomSize(){
+        return ((int)(Math.random() * 320));
+    }
+
+
+    public static void drawSquare(Graphics graphics){
+        int size = randomSize();
+        int x = (320 / 2) - (size / 2);
+        int y = (320 / 2) - (size / 2);
+
+        graphics.setColor(new Color(randomColor(), randomColor(), randomColor(), randomColor()));
+        graphics.fillRect(x,y, size, size);
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
