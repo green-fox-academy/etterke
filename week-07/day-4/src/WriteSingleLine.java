56f1af270959321266278bffc00ee11b0f6ed0f1
Etterke <etterke@gmail.com>
2018-11-16 00:16:48 +0100

    Perfected the solution :)

--- week-07/day-4/src/WriteSingleLine.java
+++ week-07/day-4/src/WriteSingleLine.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
 
 public class WriteSingleLine {
@@ -10,11 +11,11 @@ public class WriteSingleLine {
     Path filePathRel = Paths.get("src/my-name.txt");
     Path filePathAbs = Paths.get("/Users/eszternagy/greenfox/etterke/week-07/day-4/src/my-name.txt");
 
-    List<String> lines = null;
+    List<String> lines = new ArrayList<>();
+    lines.add("Eszter Nagy");
 
     try {
-      lines = Files.readAllLines(filePathAbs);
-      lines.add("Eszter Nagy");
+      Files.write(filePathAbs, lines);
       System.out.println(lines);
     } catch (IOException IOe) {
       System.out.println("Unable to write file: my-name.txt");
