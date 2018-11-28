8f6ebb48032d4283207a9195f107cde0ecb0a599
Etterke <etterke@gmail.com>
2018-11-28 10:26:48 +0100

    feat(Bunnies) created main method

---
+++ week-09/day-3/src/Bunnies.java
@@ -0,0 +1,11 @@
+public class Bunnies {
+
+  public static void main(String[] args) {
+
+    int bunnies = 5;
+    int ears = 2;
+    int result = calculateNumberOfEars(bunnies, ears);
+
+    System.out.println(result);
+  }
+}
