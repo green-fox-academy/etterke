0a98001280b8a11e88a7b689e2fc94bcc186b712
Etterke <etterke@gmail.com>
2019-01-12 15:58:36 +0100

    feat(PalindromeSearch): completed exercise, although needs imporvement

--- week-13/src/PalindromeSearcher.java
+++ week-13/src/PalindromeSearcher.java
@@ -1,27 +1,41 @@
-//import java.util.Scanner;
-//
-//public class PalindromeSearcher {
-//
-//  public static void main(String[] args) {
-//
-//    Scanner scannerString = new Scanner(System.in);
-//    System.out.println("Please, enter a word, that is at least 3 characters: ");
-//    String input = scannerString.nextLine();
-//    System.out.println(searchPalindrome(input));
-//  }
-//
-//  private static boolean checkIfPalindrome(String input) {
-//
-//    char[] word = input.toCharArray();
-//
-//    for (int i = 0; i < word.length / 2; i++) {
-//      if (word[i] != word[word.length - 1 - i]) {
-//        return false;
-//      }
-//    }
-//    return true;
-//  }
-//
-//  private static char[] searchPalindrome(String input) {
-//  }
-//}
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PalindromeSearcher {
+
+  public static void main(String[] args) {
+
+    Scanner scannerString = new Scanner(System.in);
+    System.out.println("Please, enter a word, that is at least 3 characters: ");
+    String input = scannerString.nextLine();
+    System.out.println(searchPalindrome(input));
+  }
+
+  private static boolean checkIfPalindrome(String input) {
+    char[] word = input.toCharArray();
+
+    for (int i = 0; i < word.length / 2; i++) {
+      if (word[i] == word[word.length - i - 1]) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private static ArrayList<String> searchPalindrome(String input) {
+    ArrayList<String> result = new ArrayList<>();
+    int minLength = 3;
+
+    for (int i = 0; i < input.length() - minLength + 1; i++) {
+      for (int j = i + minLength - 1; j < input.length(); j++) {
+        String subString = input.substring(i, j);
+        if (checkIfPalindrome(subString)){
+          result.add(subString);
+        }
+      }
+    }
+    return result;
+  }
+}
