3b7f2470abce3c742770ee51ac62571da71031dd
Etterke <etterke@gmail.com>
2019-01-12 16:20:36 +0100

    feat(Reverse): alternative solution with function and char[]

--- week-05/day03/src/Reverse.java
+++ week-05/day03/src/Reverse.java
@@ -7,7 +7,18 @@ public class Reverse {
         StringBuilder corrected = new StringBuilder(reversed);
 
         System.out.println(corrected.reverse().toString());
+        System.out.println(reverse(reversed));
 
+    }
+
+    public static String reverse(String text){
+        char[] reversedLetters = text.toCharArray();
+        char[] correctLetters = new char[reversedLetters.length];
+        for (int i = 0; i < reversedLetters.length; i++) {
+            System.arraycopy(reversedLetters, (reversedLetters.length - 1 - i), correctLetters, i, 1);
+        }
+        String correct = new String(correctLetters);
+        return correct;
     }
 
 
