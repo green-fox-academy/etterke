cfa53ba9bf836ca7d572d0f5791e79757df466b0
Etterke <etterke@gmail.com>
2019-02-06 00:00:57 +0100

    feat(SpringStartApp): added Hello in different languages feature, not yet able to style it

--- week-17/day-2/src/main/java/com/greenfoxacademy/springstart/Controllers/HelloWebController.java
+++ week-17/day-2/src/main/java/com/greenfoxacademy/springstart/Controllers/HelloWebController.java
@@ -1,16 +1,29 @@
 package com.greenfoxacademy.springstart.Controllers;
 
+import com.greenfoxacademy.springstart.Greeting;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 
 @Controller
 public class HelloWebController {
 
+  AtomicLong idCounter = new AtomicLong();
+
   @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World");
+  public String greeting(@RequestParam("name") String name, Model model) {
+    String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+        "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+        "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+        "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+    int randomIndex = (int) (Math.random() * 49 + 1);
+    model.addAttribute("hello", hellos[randomIndex]);
+    model.addAttribute("name", name);
+    model.addAttribute("count", idCounter.incrementAndGet());
     return "greeting";
   }
 }
