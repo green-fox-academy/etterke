8984a428ef80286379d0c2fb03b6271aaf86ccf5
Etterke <etterke@gmail.com>
2019-02-14 11:22:05 +0100

    feat(PrinterApplication): completed first exercise

--- week-18/day-4/src/main/java/com/greenfoxacademy/dependenciesexercises/PrinterApplication.java
+++ week-18/day-4/src/main/java/com/greenfoxacademy/dependenciesexercises/PrinterApplication.java
@@ -1,27 +1,29 @@
 package com.greenfoxacademy.dependenciesexercises;
 
-import com.greenfoxacademy.dependenciesexercises.Services.SpellChecker;
+import com.greenfoxacademy.dependenciesexercises.Services.Printer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpellCheckerApplication implements CommandLineRunner {
+public class PrinterApplication implements CommandLineRunner {
 
-  private SpellChecker checker;
+  private Printer printer;
 
   @Autowired
-  SpellCheckerApplication(SpellChecker checker) {
-    this.checker = checker;
+  PrinterApplication(Printer printer){
+    this.printer = printer;
   }
 
   public static void main(String[] args) {
-    SpringApplication.run(SpellCheckerApplication.class, args);
+    SpringApplication.run(DependenciesexercisesApplication.class, args);
   }
 
   @Override
   public void run(String... args) throws Exception {
-    checker.checkSpelling();
+    printer.log("Hello");
   }
 }
+
+
