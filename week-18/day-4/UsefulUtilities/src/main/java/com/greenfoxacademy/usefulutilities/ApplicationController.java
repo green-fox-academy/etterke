592a66a8172e4f2269dd4ddc49386099a5af8237
Etterke <etterke@gmail.com>
2019-02-15 11:00:31 +0100

    feat(UsefulUtilities): completed encoding and decoding features

--- week-18/day-4/UsefulUtilities/src/main/java/com/greenfoxacademy/usefulutilities/ApplicationController.java
+++ week-18/day-4/UsefulUtilities/src/main/java/com/greenfoxacademy/usefulutilities/ApplicationController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -12,30 +13,50 @@ public class ApplicationController {
   @Autowired
   private UtilityService first;
 
-  public ApplicationController(){
-
+  public ApplicationController() {
   }
 
   @GetMapping("/useful")
-  public String homePage(){
+  public String homePage() {
     return "useful";
   }
 
   @GetMapping("/useful/colored")
-  public String randomColorPage(Model model){
+  public String randomColorPage(Model model) {
     model.addAttribute("color", first.randomColor());
     return "colored";
   }
 
   @GetMapping("useful/email")
-  public String requestEmail(Model model, @RequestParam (name="email", required=false) String email){
+  public String requestEmail(Model model, @RequestParam(name = "email", required = false) String email) {
     if (email != null) {
       model.addAttribute("checkEmailValidity", first.validateEmail(email));
       model.addAttribute("email", email);
     } else {
       model.addAttribute("error", "Please give an email address!");
     }
-
     return "email";
+  }
+
+  @GetMapping("useful/{text}/{number}/encoding")
+  public String encodeText(Model model, @PathVariable(value="text") String text, @PathVariable(value="number") int number){
+    if (text != null && number != 0){
+      model.addAttribute("encode", first.caesar(text, number));
+      model.addAttribute("text", text);
+    } else {
+      model.addAttribute("error", "Please give a text and a number!");
+    }
+    return "encoding";
+  }
+
+  @GetMapping("useful/{text}/{number}/decoding")
+  public String decodeText(Model model, @PathVariable(value="text") String text, @PathVariable(value="number") int number){
+    if (text != null && number != 0){
+      model.addAttribute("decode", first.caesar(text, number));
+      model.addAttribute("text", text);
+    } else {
+      model.addAttribute("error", "Please give a text and a number!");
+    }
+    return "decoding";
   }
 }
