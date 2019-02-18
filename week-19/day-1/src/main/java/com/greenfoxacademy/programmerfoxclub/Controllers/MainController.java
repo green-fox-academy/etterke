88027ee4777ffe6134352d92e1806a8b3dda0b6f
Etterke <etterke@gmail.com>
2019-02-18 12:09:29 +0100

    feat(Programmer Fox Club):added login page and @RequestParam name

--- week-19/day-1/src/main/java/com/greenfoxacademy/programmerfoxclub/Controllers/MainController.java
+++ week-19/day-1/src/main/java/com/greenfoxacademy/programmerfoxclub/Controllers/MainController.java
@@ -1,19 +1,30 @@
 package com.greenfoxacademy.programmerfoxclub.Controllers;
 
+import com.greenfoxacademy.programmerfoxclub.Fox;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
+  Fox myFox;
 
   @GetMapping("/main")
-  public String showMainPage() {
+  public String showMainPage(Model model, @RequestParam (name="name") String name) {
+    model.addAttribute("name", name);
     return "index";
   }
 
   @GetMapping("/login")
   public String showLoginForm() {
     return "login";
+  }
+
+  @PostMapping("/login")
+  public String loginForm(Model model, @RequestParam (name="name") String name) {
+    model.addAttribute("name", name);
+    return "redirect:/main/?name=" + name;
   }
 }
---
+++ week-19/day-1/src/main/java/com/greenfoxacademy/programmerfoxclub/Fox.java
@@ -0,0 +1,38 @@
+package com.greenfoxacademy.programmerfoxclub;
+
+public class Fox {
+
+  private String name;
+  private String food;
+  private String drink;
+
+  public Fox(String name, String food, String drink) {
+    this.name = name;
+    this.food = food;
+    this.drink = drink;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getFood() {
+    return food;
+  }
+
+  public void setFood(String food) {
+    this.food = food;
+  }
+
+  public String getDrink() {
+    return drink;
+  }
+
+  public void setDrink(String drink) {
+    this.drink = drink;
+  }
+}
