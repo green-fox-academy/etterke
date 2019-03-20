c645c1693d7dd3be387bd8d9d7207d9bc8745636
Etterke <etterke@gmail.com>
2019-03-20 11:48:01 +0100

    feat(ConnectionWithMySQL): added add/edit/delete assignee function

--- week-22/day-4/ConnectionWithMySQL/src/main/java/com/greenfoxacademy/connectionwithmysql/Controllers/AssigneeController.java
+++ week-22/day-4/ConnectionWithMySQL/src/main/java/com/greenfoxacademy/connectionwithmysql/Controllers/AssigneeController.java
@@ -1,11 +1,14 @@
 package com.greenfoxacademy.connectionwithmysql.Controllers;
 
+import com.greenfoxacademy.connectionwithmysql.Models.Assignee;
 import com.greenfoxacademy.connectionwithmysql.Repositories.AssigneeRepository;
 import com.greenfoxacademy.connectionwithmysql.Services.AssigneeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/assignee")
@@ -22,7 +25,41 @@ public class AssigneeController {
   }
 
   @GetMapping("/list")
-  public String renderAssigneeList() {
+  public String renderAssigneeList(Model model) {
+    ArrayList<Assignee> assignees = assigneeService.findAllAssignees();
+    model.addAttribute("assignees", assignees);
     return "assigneelist";
+  }
+
+  @GetMapping("/add")
+  public String renderAddAssigneeForm() {
+    return "addassignee";
+  }
+
+  @PostMapping("/add")
+  public String addNewAssignee(@ModelAttribute Assignee newAssignee) {
+    assigneeService.saveNewAssignee(newAssignee);
+    return "redirect:/assignee/list";
+  }
+
+  @PostMapping("/{id}/delete")
+  public String deleteAssignee(@PathVariable long id) {
+    assigneeService.deleteAssignee(id);
+    return "redirect:/assignee/list";
+  }
+
+  @GetMapping("/{id}/edit")
+  public String renderEditAssigneesForm(@PathVariable long id, Model model) {
+    Assignee assignee = assigneeService.findById(id);
+    model.addAttribute("assignee", assignee);
+    return "editassignee";
+  }
+
+  @PostMapping("/{id}/edit")
+  public String editAssignee(@PathVariable long id, Model model) {
+    Assignee assignee = assigneeService.findById(id);
+    assigneeService.saveNewAssignee(assignee);
+    model.addAttribute("assignee", assignee);
+    return "redirect:/assignee/list";
   }
 }
--- week-22/day-4/ConnectionWithMySQL/src/main/java/com/greenfoxacademy/connectionwithmysql/Services/AssigneeService.java
+++ week-22/day-4/ConnectionWithMySQL/src/main/java/com/greenfoxacademy/connectionwithmysql/Services/AssigneeService.java
@@ -1,9 +1,38 @@
 package com.greenfoxacademy.connectionwithmysql.Services;
 
+import com.greenfoxacademy.connectionwithmysql.Models.Assignee;
+import com.greenfoxacademy.connectionwithmysql.Repositories.AssigneeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class AssigneeService {
 
+  @Autowired
+  AssigneeRepository assigneeRepo;
 
+  public ArrayList<Assignee> findAllAssignees() {
+    ArrayList<Assignee> todos = new ArrayList<>();
+    assigneeRepo.findAll().forEach(todos::add);
+    return todos;
+  }
+
+  public void saveNewAssignee(Assignee assignee){
+    assigneeRepo.save(assignee);
+  }
+
+//  public void updateAssignee(Assignee assignee) {
+//    assigneeRepo.save()
+//  }
+
+  public Assignee findById(long id) {
+    Assignee assignee = assigneeRepo.findById(id).get();
+    return assignee;
+  }
+
+  public void deleteAssignee(long id) {
+    assigneeRepo.deleteById(id);
+  }
 }
