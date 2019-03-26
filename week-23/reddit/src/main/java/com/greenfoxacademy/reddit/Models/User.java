0f7c1a9caf35739d711e7d8b8f50ce7f94e5fd03
Etterke <etterke@gmail.com>
2019-03-26 22:17:21 +0100

    feat(Navbar): bootstrapped the navbar

--- week-23/reddit/src/main/java/com/greenfoxacademy/reddit/Models/User.java
+++ week-23/reddit/src/main/java/com/greenfoxacademy/reddit/Models/User.java
@@ -41,4 +41,12 @@ public class User {
   public void setPassword(String password) {
     this.password = password;
   }
+
+  public List<Post> getPosts() {
+    return posts;
+  }
+
+  public void setPosts(List<Post> posts) {
+    this.posts = posts;
+  }
 }
