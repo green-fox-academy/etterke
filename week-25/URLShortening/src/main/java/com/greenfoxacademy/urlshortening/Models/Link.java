6d413bfa0a7489f26e1c7f55f835adb51130c002
Etterke <etterke@gmail.com>
2019-04-07 12:31:04 +0200

    feat(application.properties): ajdusted time zone, ran application, database is born

--- week-25/URLShortening/src/main/java/com/greenfoxacademy/urlshortening/Models/Link.java
+++ week-25/URLShortening/src/main/java/com/greenfoxacademy/urlshortening/Models/Link.java
@@ -1,10 +1,15 @@
 package com.greenfoxacademy.urlshortening.Models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Link {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
   private String url;
   private String alias;
--- week-25/URLShortening/src/main/java/com/greenfoxacademy/urlshortening/Repositories/LinkRepository.java
+++ week-25/URLShortening/src/main/java/com/greenfoxacademy/urlshortening/Repositories/LinkRepository.java
@@ -1,8 +1,9 @@
 package com.greenfoxacademy.urlshortening.Repositories;
 
+import com.greenfoxacademy.urlshortening.Models.Link;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LinkRepository extends CrudRepository {
+public interface LinkRepository extends CrudRepository<Link, Long> {
 }
